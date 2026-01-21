3430790c57483d210ad61207396e7f6eb069b401
Chasse_neige <3977719547@qq.com>
2026-01-21 21:15:31 +0800

    Adding calculartion for the response on the detector.

--- metricCalculate.py
+++ metricCalculate.py
@@ -1,108 +1,142 @@
 import numpy as np
-from scipy.integrate import tplquad
-from scipy.optimize import approx_fprime
+import mpmath as mp
 import math
 
+mp.mp.dps = 10  #set the precision for the code
+
 #define the contants that will be used
-H = 2   #height of the column (meter)
-D = 5   #diameter of the column (meter)
-d = 1   #diameter of the holes (meter)
-s = 1.5 #distance from the center of the colum to the center of the holes (meter)
-R = 2000    #distance from the source to the detector (meter)
-rho = 1750  #the density of typical carbonfiber (kg/m^3)
-G = 6.674E-11   #gravitational constant (m^3/kg*s^2)
-c = 2.998E8 #speed of light (m/s)
-cos = math.cos
-sin = math.sin
-pi = math.pi
-omega = 20000 * 2 * pi    #the frequency of the rotation (rad/s)
+H = mp.mpf('2')      # height of the column (meter)
+D = mp.mpf('5')      # diameter of the column (meter)
+d = mp.mpf('1')      # diameter of the holes (meter)
+s = mp.mpf('1.5')    # distance from center to holes (meter)
+R = mp.mpf('2000')   # distance from source to detector (meter)
+rho = mp.mpf('1750') # density (kg/m^3)
+G = mp.mpf('6.674e-11')  # gravitational constant
+c = mp.mpf('2.998e8')    # speed of light
+pi = mp.pi
+omega = mp.mpf('300') * mp.mpf('2') * pi  # rotation frequency
+sin = mp.sin
+cos = mp.cos
 def dirac_delta(i, j):
         if i==j:
-            return 1
+            return mp.mpf('1')
         else:
-            return 0    #dirac delta function
+            return mp.mpf('0')    #dirac delta function
+        
+def get_metricTensorcomponentTT(r, t):
+    #describe the rotation of the columns: we assume that at the time t=0, the center of one hole is at the direction of the x axis, and the center of the four holes are defined by x_k and y_k (k = 0,1)
+    def get_the_coordinate_of_the_hole(k, t):
+        x_k = s * cos(omega * t + k * pi) 
+        y_k = s * sin(omega * t + k * pi)
+        return x_k, y_k
 
-#describe the rotation of the columns: we assume that at the time t=0, the center of one hole is at the direction of the x axis, and the center of the four holes are defined by x_k and y_k (k = 1,2,3,4)
-def get_the_coordinate_of_the_hole(k, t):
-    x_k = s * cos(omega * t + k * pi/2) 
-    y_k = s * sin(omega * t + k * pi/2)
-    return x_k, y_k
+    #calculate the components of the quadrupole tensor
+    #calculate the ij componet of the quadrupole tensor of one column
+    # def calculate_one_column_tensor(i, j, D, rho, H):
+    #     def y_lower(x):
+    #         return -np.sqrt(D**2/4 - x**2)
+    #     def y_upper(x):
+    #         return np.sqrt(D**2/4 - x**2)
+    #     x_lower = -D/2
+    #     x_upper = D/2
+    #     z_lower = -H/2
+    #     z_upper = H/2
+    #     def integrand(x, y, z):
+    #         coordinates = [x, y, z]
+    #         return rho * (coordinates[i] * coordinates[j] - mp.mpf('1')/3 * dirac_delta(i, j) * (x**2 + y**2 + z**2))
+        
+    #     def f(x):
+    #         def g(y):
+    #             def h(z):
+    #                 return integrand(x, y, z)
+    #             return mp.quad(h, [z_lower, z_upper])
+    #         return mp.quad(g, [y_lower(x), y_upper(x)])
+        
+    #     component = mp.quad(f, [x_lower, x_upper])
+    #     return component
 
-#calculate the components of the quadrupole tensor
-#calculate the ij componet of the quadrupole tensor of one column
-def calculate_one_column_tensor(i, j, D, rho, H):
-    def y_lower(x):
-        return -np.sqrt(D**2/4 - x**2)
-    def y_upper(x):
-        return np.sqrt(D**2/4 - x**2)
-    def integrand(x, y, z):
-        coordinates = [x, y, z]
-        integrand = rho *(coordinates[i] * coordinates[j] - 1/3 * dirac_delta(i, j) * (x**2 + y**2 + z**2))
-        return integrand
-    component = tplquad(
-            integrand,
-            -D/2, D/2,                    
-            y_lower, y_upper,         
-            lambda x, y: -H/2,           
-            lambda x, y: H/2  
-        )[0]
-    return component
+    #calculate the componets of quadrupole tensor of the holes relative to the center of the column
+    def hole_tensor_component(i, j, k, d, rho ,H ,t):
+        component = mp.mpf('0')
+        x = get_the_coordinate_of_the_hole(k, t)[0]
+        y = get_the_coordinate_of_the_hole(k, t)[1]
+        z = mp.mpf('0')
+        def relative_component(i, j, t):
+            coordinates = [x, y, z]
+            relativeComponent = rho * pi * d**2 / 4 * H * (coordinates[i] * coordinates[j] - mp.mpf('1')/3 * dirac_delta(i, j) * (x**2 + y**2 + z**2))
+            return relativeComponent
+        component += relative_component(i, j, t)
+        return component
 
-#calculate the componets of quadrupole tensor of the holes relative to the center of the column
-def hole_tensor_component(i, j, k, d, rho ,H ,t):
-    component = calculate_one_column_tensor(i, j, d, rho, H)
-    x = get_the_coordinate_of_the_hole(k, t)[0]
-    y = get_the_coordinate_of_the_hole(k, t)[1]
-    z = 0
-    def relative_component(i, j, t):
-        coordinates = [x, y, z]
-        relativeComponent = rho * pi * d**2 / 4 * H * (coordinates[i] * coordinates[j] - 1/3 * dirac_delta(i, j) * (x**2 + y**2 + z**2))
-        return relativeComponent
-    component += relative_component(i, j, t)
-    return component
+    #adding to get the whole quadrupole tensor I_ij
+    def calculate_the_whole_tensor(i, j, D, rho, H, t):
+        big_column = mp.mpf('0')
+        component = big_column
+        for k in range(2):
+            small_column = hole_tensor_component(i, j, k, d, -rho, H, t)
+            component += small_column
+        return component
 
-#adding to get the whole quadrupole tensor I_ij
-def calculate_the_whole_tensor(i, j, D, rho, H, t):
-    big_column = calculate_one_column_tensor(i, j, D, rho, H)
-    component = big_column
-    for k in range(4):
-        small_column = hole_tensor_component(i, j, k, d, -rho, H, t)
-        component += small_column
-    return component
+    #calculate the second-order derivative of the quadrupole tensor
+    def second_derivative_of_component(i, j, t):
+        if i==2 or j==2:
+            return mp.mpf('0')
+        else:
+            derivative = - mp.mpf('4') * omega ** 2 * calculate_the_whole_tensor(i, j, D, rho, H, t)
+            return derivative
 
-#calculate the second-order derivative of the quadrupole tensor
-def second_derivative_of_component(i, j, t):
-    if i==2 or j==2:
-        return 0
-    else:
-        derivative = - 4 * omega ** 2 * calculate_the_whole_tensor(i, j, D, rho, H, t)
-        return derivative
-
-#get the final metric tensor
-t = float(input("current time:"))
-t_rev = t - R/c
-metricTensorComponent = [[0,0,0],[0,0,0],[0,0,0]]
-for i in range(3):
-    for j in range(3):
-        metricTensorComponent[i][j] = 2 * G * second_derivative_of_component(i, j, t_rev) / (R * c**4)
-        print(metricTensorComponent[i][j], end = " ")
-    print("\t")
-
-#project the mectric tensor to TT mode
-def project_to_TT_mode(metricTensorComponent):
-    projector = [[1,0,0],[0,1,0],[0,0,0]]
-    metricTensorComponentTT = [[0,0,0],[0,0,0],[0,0,0]]
+    #get the final metric tensor
+    t_rev = t - R/c
+    metricTensorComponent = [[mp.mpf('0') for _ in range(3)] for _ in range(3)]
     for i in range(3):
         for j in range(3):
-            for k in range(3):
-                for l in range(3):
-                    metricTensorComponentTT[i][j] += (projector[i][k] * projector[j][l] - 0.5 * projector[i][j] * projector[k][l]) * metricTensorComponent[k][l]
+            metricTensorComponent[i][j] = mp.mpf('2') * G * second_derivative_of_component(i, j, t_rev) / (r * c**4)
+
+    #project the mectric tensor to TT mode
+    def project_to_TT_mode(metricTensorComponent):
+        projector = [[mp.mpf('1'), mp.mpf('0'), mp.mpf('0')],
+                    [mp.mpf('0'), mp.mpf('1'), mp.mpf('0')],
+                    [mp.mpf('0'), mp.mpf('0'), mp.mpf('0')]]
+        metricTensorComponentTT = [[mp.mpf('0') for _ in range(3)] for _ in range(3)]
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    for l in range(3):
+                        metricTensorComponentTT[i][j] += (projector[i][k] * projector[j][l] - mp.mpf('0.5') * projector[i][j] * projector[k][l]) * metricTensorComponent[k][l]
+        return metricTensorComponentTT
+
+    #output the metric tensor in TT gauge
+    metricTensorComponentTT = project_to_TT_mode(metricTensorComponent)
     return metricTensorComponentTT
 
-#output the final
-metricTensorComponentTT = project_to_TT_mode(metricTensorComponent)
-for i in range(3):
-    for j in range(3):
-        print(metricTensorComponentTT[i][j], end = " ")
-    print("\t")
+#calculate the average respond for monochromatic wave
+#define the orientation of the detector
+theta_arm = pi / 4
+phi_arm = pi / 4    #the orientation of one arm of the detector (rad)
+theta_det = mp.mpf('1.3')
+phi_det = mp.mpf('1.4')   #the orientation of the vector from the centor of the source to the center of the detector
+a_i, a_j, a_k = sin(theta_arm) * cos(phi_arm), sin(theta_arm) * sin(phi_arm), cos(theta_arm)
+n_i, n_j, n_k = sin(theta_det) * cos(phi_det), sin(theta_det) * sin(phi_det), cos(theta_det)
+L = mp.mpf('1000')    #length of the arm of the detector (meter)
 
+
+def calculate_deltaT(t):
+    deltaT = mp.mpf('0')
+    def distance(x):    #return the distance from the center of the source to the position of the photon
+        return mp.sqrt((R * n_i + x * a_i)**2 + (R * n_j + x * a_j)**2 + (R * n_k + x * a_k)**2)
+    for i in range(3):
+        for j in range(3):
+            deltaT += integrate.quad(lambda x: mp.mpf('1') / (2*c) * a_i * a_j * get_metricTensorcomponentTT(distance(x), t + x / c)[i][j], mp.mpf('0'), L)[0]
+    return deltaT
+
+def calculate_deltaTPrime(t):
+    deltaTPrime = mp.mpf('0')
+    def distance(x):    #return the distance from the center of the source to the position of the photon
+        return mp.sqrt((R * n_i + x * a_i)**2 + (R * n_j + x * a_j)**2 + (R * n_k + x * a_k)**2)
+    for i in range(3):
+        for j in range(3):
+            deltaTPrime += integrate.quad(lambda x: mp.mpf('1') / (2*c) * a_i * a_j * get_metricTensorcomponentTT(distance(L - x), t + (L - x) / c)[i][j], mp.mpf('0'), L)[0]
+    return deltaTPrime
+
+t = float(input("current time:"))
+print(calculate_deltaT(t), calculate_deltaTPrime(t))
